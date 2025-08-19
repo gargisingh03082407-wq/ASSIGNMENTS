c2ed00071684f5fb1772f10ed23317731d104d82
Gargi Singh <gargisingh03082407@gmail.com>
2025-08-19 22:33:13 +0530

    Add files via upload

---
+++ assignment4.py
@@ -0,0 +1,10 @@
+# Prime numbers between 1 and 100
+
+for num in range(2, 101):   # 1 is not prime
+    is_prime = True
+    for i in range(2, int(num ** 0.5) + 1):  # check till sqrt(num)
+        if num % i == 0:
+            is_prime = False
+            break
+    if is_prime:
+        print(num, end=" ")
